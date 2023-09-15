4f7dc90559601f143f2045a79d2f33f0e6b3c925
Wocke03 <julian.areizagallego@gmail.com>
2023-09-15 01:18:42 -0500

    Configuracion del proyecto

--- SistemaInventario/obj/Debug/net7.0/SistemaInventario.AssemblyInfo.cs
+++ SistemaInventario/obj/Debug/net7.0/SistemaInventario.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dec3f10ada8649470b499442fd822cc8825a337b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
